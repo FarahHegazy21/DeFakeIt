e8b76dabcc77b7a25d081e9dc0feb6996fa0bf1d
Farah Hegazy <cds.farahmohamed52894@alexu.edu.eg>
2025-03-19 04:38:02 +0200

    The structure of the app

---
+++ lib/main.dart
@@ -0,0 +1,23 @@
+import 'package:defakeit/Themes/theme.dart';
+import 'package:defakeit/nav_view.dart';
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      theme: AppTheme.lightTheme,
+      darkTheme: AppTheme.darkTheme,
+      themeMode: ThemeMode.system,
+      home: NavView(),
+    );
+  }
+}
---
+++ lib/nav_view.dart
@@ -0,0 +1,118 @@
+import 'package:defakeit/Themes/theme.dart';
+import 'package:defakeit/screens/analysis_history_screen.dart';
+import 'package:defakeit/screens/home_screen.dart';
+import 'package:defakeit/screens/profile_screen.dart';
+import 'package:defakeit/screens/settings_screen.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+
+class NavView extends StatefulWidget {
+  const NavView({super.key});
+
+  @override
+  State<NavView> createState() => _NavViewState();
+}
+
+class _NavViewState extends State<NavView> {
+  int _selectedIndex = 0;
+
+  @override
+  Widget build(BuildContext context) {
+    bool isDarkMode = Theme.of(context).brightness == Brightness.dark;
+
+    // Set status bar color dynamically based on theme mode
+    SystemChrome.setSystemUIOverlayStyle(SystemUiOverlayStyle(
+      statusBarColor: Colors.transparent,
+      statusBarIconBrightness: isDarkMode ? Brightness.light : Brightness.dark,
+    ));
+
+    return Scaffold(
+      body: Stack(
+        children: [
+          Positioned(
+            top: 0,
+            right: 0,
+            left: 0,
+            child: Image.asset(
+              "assets/images/background_home_transparent.png",
+              fit: BoxFit.cover,
+              color: isDarkMode ? Colors.white.withOpacity(0.2) : null,
+            ),
+          ),
+          SafeArea(
+            child: IndexedStack(
+              index: _selectedIndex,
+              children: const [
+                HomeScreen(),
+                AnalysisHistoryScreen(),
+                ProfileScreen(),
+                SettingsScreen(),
+              ],
+            ),
+          ),
+        ],
+      ),
+      bottomNavigationBar: Padding(
+        padding: const EdgeInsets.all(18.0),
+        child: Container(
+          decoration: BoxDecoration(
+            color: AppTheme.primaryColor,
+            borderRadius: BorderRadius.circular(30),
+            boxShadow: [
+              BoxShadow(
+                color: Colors.black.withOpacity(0.1),
+                blurRadius: 10,
+                spreadRadius: 2,
+                offset: const Offset(0, 4),
+              )
+            ],
+          ),
+          child: ClipRRect(
+            borderRadius: BorderRadius.circular(30),
+            child: BottomNavigationBar(
+              backgroundColor:
+                  Theme.of(context).bottomNavigationBarTheme.backgroundColor,
+              currentIndex: _selectedIndex,
+              onTap: (index) {
+                setState(() {
+                  _selectedIndex = index;
+                });
+              },
+              selectedLabelStyle: const TextStyle(fontSize: 12),
+              unselectedLabelStyle: const TextStyle(fontSize: 12),
+              type: BottomNavigationBarType.fixed,
+              items: [
+                BottomNavigationBarItem(
+                  icon: _buildNavIcon("assets/images/Home.png", 0),
+                  label: "Home",
+                ),
+                BottomNavigationBarItem(
+                  icon: _buildNavIcon("assets/images/Analysis History.png", 1),
+                  label: "Analysis History",
+                ),
+                BottomNavigationBarItem(
+                  icon: _buildNavIcon("assets/images/profile.png", 2),
+                  label: "Profile",
+                ),
+                BottomNavigationBarItem(
+                  icon: _buildNavIcon("assets/images/Settings.png", 3),
+                  label: "Settings",
+                ),
+              ],
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+
+  Widget _buildNavIcon(String assetPath, int index) {
+    return ColorFiltered(
+      colorFilter: ColorFilter.mode(
+        _selectedIndex == index ? const Color(0xFF52859E) : Colors.grey,
+        BlendMode.srcIn,
+      ),
+      child: Image.asset(assetPath, width: 24, height: 24),
+    );
+  }
+}
